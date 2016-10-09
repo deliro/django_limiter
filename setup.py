1f452322c21be8665e82317eb09aaeb7e587d910
Roman Kitaev <t4k.kitaetz@gmail.com>
2016-10-09 10:38:12 +0700

    fixed templates upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Signed Cookie based limiting RPS module for Django',
     keywords='django rps limit',
     packages=find_packages(),
-    version='1.0.1',
+    version='1.0.2',
     author='Roman Kitaev',
     author_email='t4k.kitaetz@gmail.com',
     url='https://github.com/deliro/django_limiter',
